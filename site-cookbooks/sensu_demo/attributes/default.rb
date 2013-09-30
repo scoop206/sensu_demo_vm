8e60576d660952b83c710e5379007b60555cc7b7
Sam Cooper <scooper@bluebox.net>
2013-09-30 05:44:41 -0700

    dashboard bound to 0.0.0.0

--- site-cookbooks/sensu_demo/attributes/default.rb
+++ site-cookbooks/sensu_demo/attributes/default.rb
@@ -4,6 +4,6 @@ node.default.sensu_demo.rabbitmq.host        = "localhost"
 node.default.sensu_demo.rabbitmq.password    = "password"
 node.default.sensu_demo.redis.host           = "localhost"
 node.default.sensu_demo.api.host             = "localhost"
-node.default.sensu_demo.dashboard.bind       = "localhost"
+node.default.sensu_demo.dashboard.bind       = "0.0.0.0"
 node.default.sensu_demo.dashboard.password   = "foolhardy"
 
