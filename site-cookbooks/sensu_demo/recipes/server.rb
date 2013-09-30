6b520ca55afbee85bb6e13e1e4dfd674c1a0c38c
Sam Cooper <scooper@bluebox.net>
2013-09-30 05:23:42 -0700

    don't try and start sendmail

--- site-cookbooks/sensu_demo/recipes/server.rb
+++ site-cookbooks/sensu_demo/recipes/server.rb
@@ -9,6 +9,3 @@ include_recipe "sensu::dashboard_service"
 # include_recipe "sensu_demo::handlers"
 # include_recipe "sensu_demo::sensu_server_checks"
 
-service "sendmail" do
-  action [ :enable, :start ]
-end
